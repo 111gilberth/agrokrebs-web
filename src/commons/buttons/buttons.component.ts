ec992b98bc9212bad4a29440d7c073413e691183
Gilberth ord snc <r.o.t.o.beto@gmail.com>
2020-09-04 12:49:02 -0500

    form, input added to project

--- src/commons/buttons/buttons.component.ts
+++ src/commons/buttons/buttons.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input, EventEmitter } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
     @Input() buttonType: string;
-    @Input() buttonLabel: string = 'Aceptar';
+    @Input() buttonLabel: string;
     @Input() buttonClass: string;
     @Input() iconClass:  string;
     @Input() click: EventEmitter<string> = new EventEmitter<string>();
@@ -17,15 +17,18 @@ export class ButtonsComponent implements OnInit {
   public ngOnInit(): void {
       switch(this.buttonClass){
           case "cancel":{
-            this.buttonClass ="btn-cancel"
+            this.buttonClass = "btn-cancel"
+            this.buttonLabel = "Cancelar"
             break;
           }
           case "success":{
-            this.buttonClass =""
+            this.buttonClass ="btn-success"
+            this.buttonLabel = "Aceptar"
             break;
           }
           case "delete":{
             this.buttonClass ="btn-delete"
+            this.buttonLabel = "Eliminar"
             break;
           }
       }
