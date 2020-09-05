207e43b23c4e3033d2397232eb764b60c2bc9395
Gilberth ord snc <r.o.t.o.beto@gmail.com>
2020-09-05 14:09:40 -0500

    changed applied to commons/form

--- src/commons/form/form.component.ts
+++ src/commons/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormControlDirective } from '@angular/forms'
 
 @Component({
   selector: 'app-form',
@@ -6,8 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  contactForm: FormGroup
+  estados = []
+  requestTypes = ['Claim', 'Feedback', 'Help Request']
+  municipios = []
 
-  constructor() { }
+  constructor() {
+    this.contactForm = this.createContactFormGroup()
+   }
+
+   createContactFormGroup(){
+     return new FormGroup({
+            personalData: new FormGroup({
+                id: new FormControl(),
+                nombre: new FormControl(),
+                correo: new FormControl(),
+                idMunicipio: new FormControl(),
+                idEstado: new FormControl(),
+                cp: new FormControl()
+            }),
+              requestType: new FormControl(),
+              mensaje: new FormControl(),
+     })
+
+   }
 
   ngOnInit(): void {
   }
