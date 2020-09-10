ea987e8648f57eddc9d7fb53169d6a24877a316e
Gilberth ord snc <r.o.t.o.beto@gmail.com>
2020-09-10 18:00:11 -0500

    save without error in footer component

---
+++ src/commons/footer/footer.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+  @Input()  footerStyle: string;
+  @Input()  footerText: string;
+  @Input()  gridRow: string;
+  @Input()  fooName: string;
+
+  constructor() { }
+
+  public ngOnInit(): void {
+      switch(this.fooName){
+            case "footerUno":{
+                this.footerStyle = "page-footer font-small special-color-dark pt-4"
+                this.footerText = "container-fluid text-center text-md-left"
+                this.gridRow = "row"
+              break;
+            }
+            case "footerDos":{
+                this.footerStyle = "page-footer font-small teal pt-4"
+                this.footerText = "container-fluid text-center text-md-left"
+                this.gridRow = "row"
+              break;
+            }
+            case "footerTres":{
+                this.footerStyle = "page-footer font-small indigo"
+                this.footerText = "container text-center text-md-left"
+                this.gridRow = "row"
+              break;
+           }
+       }
+    }
+}
